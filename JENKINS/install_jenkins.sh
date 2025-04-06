68e3d93713756378c399e2a80dffc64972ba1b02
AnthonyIfy <136186728+AnthonyIfy@users.noreply.github.com>
2025-04-06 08:26:35 -0500

    Update install_jenkins.sh

--- JENKINS/install_jenkins.sh
+++ JENKINS/install_jenkins.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 sudo apt update
-sudo apt install openjdk-17-jdk -y
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 63667EE74BBA1F0A08A698725BA31D57EF5975CA
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install openjdk-17-jre-headless -y
 sudo apt install ca-certificates
 sudo apt update
-sudo apt install git -y
 sudo apt install maven -y
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install jenkins -y
 sudo apt-get update -y
@@ -21,21 +20,10 @@ gpg --no-default-keyring \
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
     https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
     sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update -y
-sudo apt install terraform -y
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-key fingerprint 0EBFCD88
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
-sudo usermod -aG docker jenkins
-sudo usermod -aG docker ubuntu
-sudo usermod -aG docker $USER
-sudo reboot
 sudo apt update
 sudo apt install terraform -y
 echo 'clearing screen...' && sleep 5
 clear
 echo 'jenkins is installed'
 echo 'this is the default password :' $(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+sudo reboot
