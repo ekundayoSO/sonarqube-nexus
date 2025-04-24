a659fe9a0cca050d1dce6dfd24becd016659e34c
AnthonyIfy <136186728+AnthonyIfy@users.noreply.github.com>
2025-04-23 22:44:11 -0500

    Update install_tomcat.sh

--- tomcat/install_tomcat.sh
+++ tomcat/install_tomcat.sh
@@ -1,19 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+
+# Update and install Java & Tomcat 9
 sudo apt update -y
 sudo apt install openjdk-11-jdk -y
 sudo apt-get install tomcat9 tomcat9-docs tomcat9-admin -y
+
+# Copy admin webapps if needed (usually already deployed)
 sudo cp -r /usr/share/tomcat9-admin/* /var/lib/tomcat9/webapps/ -v
+
+# Backup tomcat-users.xml
+sudo cp /var/lib/tomcat9/conf/tomcat-users.xml /var/lib/tomcat9/conf/tomcat-users.xml.bak
+
+# Insert roles & users before closing </tomcat-users>
+sudo sed -i '/<\/tomcat-users>/i \
+<role rolename="manager-script"/>\n\
+<role rolename="admin-gui"/>\n\
+<role rolename="manager-gui"/>\n\
+<user username="tomcat" password="password" roles="manager-script"/>\n\
+<user username="admin" password="admin" roles="admin-gui,manager-gui"/>' /var/lib/tomcat9/conf/tomcat-users.xml
+
+# Set appropriate permissions
 sudo chmod 777 /var/lib/tomcat9/conf/tomcat-users.xml
-sudo cat <<EOF >> /var/lib/tomcat9/conf/tomcat-users.xml
-<role rolename="manager-script"/>
-<user username="tomcat" password="password" roles="manager-script"/>
-<role rolename="admin-gui"/>
-<role rolename="manager-gui"/>
-<user username="admin" password="admin" roles="admin-gui,manager-gui"/>
-</tomcat-users>
-EOF
-sudo sed -i '44d' /var/lib/tomcat9/conf/tomcat-users.xml
-echo 'clearing screen...' && sleep 5
-clear
-echo 'tomcat is installed'
+sudo chown root:tomcat /var/lib/tomcat9/conf/tomcat-users.xml
+
+# Restart Tomcat
 sudo systemctl restart tomcat9
+
+echo 'Tomcat 9 installed and configured successfully.'
